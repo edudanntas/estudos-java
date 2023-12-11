d56a504baddae00bd995d8cb5071d97a0d2e74ba
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-11 12:31:21 -0300

    commit inicial: Criação de projeto e estudo de packages

---
+++ src/edu/eduardo/primeirasemana/MinhaClasse.java
@@ -0,0 +1,25 @@
+package edu.eduardo.primeirasemana;
+
+import java.util.Scanner;
+
+public class MinhaClasse{
+    public static void main(String[]args){
+        Scanner scan;
+        scan = new Scanner(System.in);
+        String nomeUm, nomeDois;
+
+        System.out.println("Digite seu primeiro nome: ");
+        nomeUm = scan.next();
+        System.out.println("Digite seu segundo nome: ");
+        nomeDois = scan.next();
+
+        String nomeCompleto = nomeCompleto(nomeUm, nomeDois);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String nomeUm, String nomeDois){
+        return nomeUm.concat(" ").concat(nomeDois);
+    }
+}
