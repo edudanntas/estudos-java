3b91b48bcd547beb7ecbf5e30657c75fc2fc643f
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-12 14:43:29 -0300

    Adicionando Classe calculadora com comentários de documentação

---
+++ src/edu/eduardo/primeirasemana/docs/Calculadora.java
@@ -0,0 +1,22 @@
+package edu.eduardo.primeirasemana.docs;
+
+/**
+ * <h1>Calculadora</h1>
+ * <p>Classe que irá fazer uma calculadora onde terão os números somados</p>
+ * <b>Note:</b> Leia atentamente a documentação para melhor entendimento do programa
+ * @author Eduardo Dantas
+ * @version 1.0
+ * @since 12/12/2023
+ * */
+
+public class Calculadora {
+    /**
+     * Método que faz a soma de dois numeros
+     * @param numeroUm Primeiro parametro do método, ele necessita ser um número inteiro e será somado ao segundo parametro
+     * @param numeroDois Segundo parametro do método, ele necessita ser um número inteiro e será somado ao primeoro parametro
+     * @return Retorna a soma do parametro um e parametro dois
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
