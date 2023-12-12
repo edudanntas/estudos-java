61d7e936750395651b4b77a9bc66cc23e57494c7
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-12 19:09:32 -0300

    add SistemaBancario.java to project

---
+++ src/edu/eduardo/primeirasemana/fluxos/condicionais/loops/SistemaBancario.java
@@ -0,0 +1,53 @@
+package edu.eduardo.primeirasemana.fluxos.condicionais.loops;
+
+import java.util.Scanner;
+
+public class SistemaBancario {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double saldo = 0;
+        while (true) {
+            int opcao;
+            System.out.println("Selecione uma opção:");
+            System.out.println("1: Deposito");
+            System.out.println("2: Saque");
+            System.out.println("3: Extrato");
+            System.out.println("0: para o programa");
+            opcao = scan.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println(" ");
+                    System.out.println("Digite um valor para depositar: ");
+                    double valorDeposito = scan.nextDouble();
+                    if (valorDeposito > 0) {
+                        saldo += valorDeposito;
+                        System.out.println("Valor de: " + valorDeposito + " foi depositado com sucesso");
+                        System.out.println(" ");
+                        System.out.println("Seu saldo atual é de: " + "R$" + saldo);
+                    } else {
+                        System.out.println("Valor de depósito inválido");
+                    }
+                    break;
+                case 2:
+                    System.out.println(" ");
+                    System.out.println("Digite um valor para Sacar: ");
+                    double valorSaque = scan.nextDouble();
+                    if (valorSaque > 0 && valorSaque < saldo) {
+                        saldo -= valorSaque;
+                        System.out.println("Valor de: " + valorSaque + " foi sacado com sucesso");
+                        System.out.println(" ");
+                        System.out.println("Seu saldo atual é de: " + "R$" + saldo);
+                    } else {
+                        System.out.println("Valor de saque inválido");
+                    }
+                    break;
+                case 3:
+                    System.out.println("O seu saldo é de: " + saldo);
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+}
