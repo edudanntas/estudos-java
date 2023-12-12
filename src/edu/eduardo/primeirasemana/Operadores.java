67a98f5e7fb6062fc06f32a90a282a1e04c6cd8b
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-12 08:51:52 -0300

    adição do arquivo Operadores

---
+++ src/edu/eduardo/primeirasemana/Operadores.java
@@ -0,0 +1,32 @@
+package edu.eduardo.primeirasemana;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // Aritméticos:
+
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+    }
+}
