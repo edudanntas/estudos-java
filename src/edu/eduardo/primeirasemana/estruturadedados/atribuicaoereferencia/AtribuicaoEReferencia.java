942352357e3b6b59a51b99112d1fb97deb7ad076
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-26 15:43:08 -0300

    AtribuicaoEReferencia de memoria

---
+++ src/edu/eduardo/primeirasemana/estruturadedados/atribuicaoereferencia/AtribuicaoEReferencia.java
@@ -0,0 +1,8 @@
+package edu.eduardo.primeirasemana.estruturadedados.atribuicaoereferencia;
+
+public class AtribuicaoEReferencia {
+    public static void main(String[] args) {
+        int intA = 1;
+        int intB = intA;
+    }
+}
