85057c34e3c253fb9df5cb05f83b21ed91ac256b
Eduardo Dantas <eduardodanttas@icloud.com>
2024-02-01 17:16:36 -0300

    Criação dos getters e setters

--- src/edu/eduardo/segundasemana/poo/Carro.java
+++ src/edu/eduardo/segundasemana/poo/Carro.java
@@ -14,5 +14,28 @@ public class Carro {
         this.capacidadeTanque = capacidadeTanque;
     }
 
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
 
 }
