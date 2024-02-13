6622ecf960ed53743b1ef696b4dcf8a56ef226ab
Eduardo Dantas <eduardodanttas@icloud.com>
2024-02-13 18:19:05 -0300

    Criação da classe Gato para implementar ordenacao

---
+++ src/edu/eduardo/terceirasemana/collections/list/ordenada/Gato.java
@@ -0,0 +1,39 @@
+package edu.eduardo.terceirasemana.collections.list.ordenada;
+
+public class Gato implements Comparable<Gato>{
+    private String nome;
+    private int idade;
+    private String cor;
+
+    public Gato(String nome, int idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Gato o) {
+        return this.getNome().compareToIgnoreCase(o.getNome());
+    }
+}
